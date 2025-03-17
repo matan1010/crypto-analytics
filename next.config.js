7986c3e31e8ee51e6b00a2d32adc9c0a7cf5a13a
matan1010 <kimhifood@gmail.com>
2025-03-18 01:27:07 +0200

    תיקון בעיות בנייה - עדכון הגדרות Next.js ושילוב SWC

--- next.config.js
+++ next.config.js
@@ -2,6 +2,24 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  webpack: (config) => {
+    config.externals = [...(config.externals || []), { canvas: 'canvas' }];
+    
+    // Add a rule for .mjs files
+    config.module.rules.push({
+      test: /\.mjs$/,
+      include: /node_modules/,
+      type: 'javascript/auto',
+    });
+
+    return config;
+  },
+  // Enable transpilePackages
+  transpilePackages: ['lightweight-charts', '@tanstack/react-query'],
+  // Disable SWC minify if causing problems
+  experimental: {
+    // esmExternals: true
+  }
 };
 
 module.exports = nextConfig; 
